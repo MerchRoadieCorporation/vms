bff6121145fa4b31e75037533ce39564cd8d46b9
hbvbot <bjviloria104@gmail.com>
2018-06-19 15:03:11 -0700

    Initial setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  render () {
+    return (
+      <div>
+        TEST
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+let app = express();
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+// app.use('/', router)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Port is ${port}`);
+});
