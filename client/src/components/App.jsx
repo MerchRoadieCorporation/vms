81bb48a28b62f291ad20f6552077d4e7ba4f6dc3
hbvbot <bjviloria104@gmail.com>
2018-06-20 17:54:11 -0700

    Page renders, added Axios

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
 
+    this.click = this.click.bind(this);
+  }
+
+  click() {
+    const context = this;
+    Axios({
+      method: 'post',
+      url: '/test'
+    }).then(res => {
+      console.log(res);
+    })
   }
 
   render () {
