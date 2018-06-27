b73374177bdade4dec03be079695290d0c4614d8
hbvbot <bjviloria104@gmail.com>
2018-06-26 18:35:10 -0700

    Initial Landing Page Created

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -8,7 +8,37 @@ class LandingPage extends React.Component {
     render() {
       return (
         <div>
-          <h1>LANDING PAGE</h1>
+      
+          <div id="page-wrapper">
+            {/* <header id="header" className="alt">
+              <h1><a href="index.html">Spectral</a></h1>
+              <nav id="nav">
+                <ul>
+                  <li className="special">
+                    <a href="#menu" className="menuToggle"><span>Menu</span></a>
+                    <div id="menu">
+                      <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="generic.html">Generic</a></li>
+                        <li><a href="elements.html">Elements</a></li>
+                        <li><a href="#">Sign Up</a></li>
+                        <li><a href="#">Log In</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </nav>
+            </header> */}
+
+            <section id="banner">
+              <div className="inner">
+                <h2>Merch Roadie</h2>
+                <ul className="actions special">
+                  <li><a href="#" className="button primary">Log In</a></li>
+                </ul>
+              </div>
+            </section>
+          </div>
         </div>
       )
     }
