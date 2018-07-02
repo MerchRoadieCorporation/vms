d09b667fa204aaa9780f6cfd02c8cd18711867d7
hbvbot <bjviloria104@gmail.com>
2018-07-02 13:53:54 -0700

    Schema created

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,9 +3,16 @@ CREATE DATABASE mrvms;
 USE mrvms;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT NOT NULL,
+  userid INT AUTO_INCREMENT NOT NULL,
+  email VARCHAR(255),
+  PRIMARY KEY (userid)
 );
 
 CREATE TABLE sales (
-  id INT AUTO_INCREMENT NOT NULL,
+  saleid INT AUTO_INCREMENT NOT NULL,
+  device VARCHAR(255),
+  total VARCHAR(255),
+  numsold INT,
+  user VARCHAR(255),
+  PRIMARY KEY (saleid)
 )
