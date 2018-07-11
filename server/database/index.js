ad3e9d0e8743989fc687378b5247b4090142a60c
hbvbot <bjviloria104@gmail.com>
2018-07-11 11:37:34 -0700

    Able to retrieve data from Google APIs now

--- server/database/index.js
+++ server/database/index.js
@@ -14,40 +14,27 @@ client.connect(err => {
   }
 });
 
-// If modifying these scopes, delete credentials.json.
 const SCOPES = [
   'https://www.googleapis.com/auth/drive.metadata.readonly',
   'https://www.googleapis.com/auth/drive',
   'https://www.googleapis.com/auth/spreadsheets'];
 const TOKEN_PATH = 'credentials.json';
 
-// // Load client secrets from a local file.
-// fs.readFile('client_secret.json', (err, content) => {
-//   if (err) return console.log('Error loading client secret file:', err);
-//   // Authorize a client with credentials, then call the Google Drive API.
-  const clientJSON = {
-    "installed": {
-    "client_id":process.env.client_id,
-    "project_id":"merch-roadie",
-    "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-    "token_uri":"https://accounts.google.com/o/oauth2/token",
-    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-    "client_secret":process.env.client_secret,
-    "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
-    }
+// Authorize a client with credentials, then call the Google Drive API.
+const clientJSON = {
+  "installed": {
+  "client_id":process.env.client_id,
+  "project_id":"merch-roadie",
+  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "token_uri":"https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+  "client_secret":process.env.client_secret,
+  "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
   }
+}
 
-  // authorize(clientJSON, listFiles);
-  authorize(clientJSON, listMajors)
-// });
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
+// Create an OAuth2 client with the given credentials, and then execute the given callback function.
+const authorize = (credentials, callback) => {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
@@ -60,13 +47,9 @@ function authorize(credentials, callback) {
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
+// Get and store new token after prompting for user authorization, and then
+// execute the given callback with the authorized OAuth2 client.
+const getAccessToken = (oAuth2Client, callback) => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -91,12 +74,9 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 
-function listFiles(auth) {
+// Lists the names and IDs of up to 1000 files
+const listFiles = auth => {
   const fileIds = [];
   const drive = google.drive({version: 'v3', auth});
   drive.files.list({
@@ -106,7 +86,6 @@ function listFiles(auth) {
     if (err) return console.log('The API returned an error: ' + err);
     const files = res.data.files;
     if (files.length) {
-      console.log('Files:');
       files.map((file) => {
         if ((file.mimeType === 'application/vnd.google-apps.spreadsheet') && (file.name.slice(-4) === '.csv') && (file.name.slice(0, 5) === 'TRANS')) {
           fileIds.push([file.name, file.id]);
@@ -115,33 +94,50 @@ function listFiles(auth) {
     } else {
       console.log('No files found.');
     }
+    return fileIds
   });
-  return fileIds
 }
 
+// Retrieve sales data from Google Sheets API
+const getSales = auth => {
+  const fileIds = [];
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+    pageSize: 1000,
+    fields: 'nextPageToken, files(id, name, mimeType)',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const files = res.data.files;
+    if (files.length) {
+      files.map((file) => {
+        if ((file.mimeType === 'application/vnd.google-apps.spreadsheet') && (file.name.slice(-4) === '.csv') && (file.name.slice(0, 5) === 'TRANS')) {
+          fileIds.push([file.name, file.id]);
+        }
+      });
+    } else {
+      console.log('No files found.');
+    }
 
-
-function listMajors(auth) {
-
-
-  const sheets = google.sheets({version: 'v4', auth});
-  // for (let i = 0; i < fileIds.length; i++) {
-  console.log(listFiles(auth))
-    sheets.spreadsheets.values.get({
-      spreadsheetId: '14i8Thcj8CYHhZJcW9KJ5rNx_XhcsxevgxxrkgNAjQE8',
-      range: 'a'
-    }, (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const rows = res.data.values;
-      if (rows.length) {
-        rows.map((row) => {
-          console.log(`MACHINE ${row[0]}, TOTAL ${row[4]}, NUMSOLD ${row[6]}`);
-        });
-      } else {
-        console.log('No data found.');
-      }
-    });
-  // }
+    const sheets = google.sheets({version: 'v4', auth});
+    for (let i = 0; i < fileIds.length; i++) {
+      sheets.spreadsheets.values.get({
+        spreadsheetId: fileIds[i][1],
+        range: fileIds[i][0]
+      }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+          rows.map((row) => {
+            console.log(`CARD_READER ${row[0]}, TOTAL_SALE ${row[4]}, NUM_SOLD ${row[6]}`);
+          });
+        } else {
+          console.log('No data found.');
+        }
+      });
+    }
+  });
 }
+
+authorize(clientJSON, getSales);
 
 module.exports = client;
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,17 +3,10 @@ CREATE DATABASE mrvms;
 USE mrvms;
 
 CREATE TABLE sales (
-  saleid INT PRIMARY KEY NOT NULL,
-  device VARCHAR(255),
-  total VARCHAR(255),
-  numsold INT,
+  saleid SERIAL PRIMARY KEY,
+  machine VARCHAR(255),
+  card_reader VARCHAR(255),
+  total_sale VARCHAR(255),
+  num_sold INT,
   email VARCHAR(255),
-);
-
-CREATE TABLE sales (
-  sale_id     SERIAL PRIMARY KEY,
-  device     VARCHAR(50),
-  total      VARCHAR(50),
-  numsold     INT,
-  email       VARCHAR(50)
 );
