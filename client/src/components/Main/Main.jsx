5c04a83427de409a50e8a95db4c739593e60bd42
hbvbot <bjviloria104@gmail.com>
2018-07-11 14:40:43 -0700

    Initial front end sales data render from dummy data

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import swal from 'sweetalert2';
+import CountUp from 'react-countup';
+
+let x = 0
+const add = () => { x+= 20; return x }
+let n = 0
+const increment = () => { n++; return n; }
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         device: '',
-        total: '',
-        numsold: '',
+        prevTotal: 0,
+        total: 0,
+        prevNumsold: 0,
+        numsold: 0,
+        showSales: false,
     }
     this.logout = this.logout.bind(this);
+    this.showSales = this.showSales.bind(this);
   }
 
   componentDidMount() {
@@ -39,8 +49,12 @@ class Main extends React.Component {
         console.log(res.data);
 
         let device;
-        let total = 0;
-        let numsold = 0;
+        let prevTotal = total
+        let prevNumsold = numsold
+        let total = add();
+        let numsold = increment();
+        // let total = 0;
+        // let numsold = 0;
 
         for(let i = 0; i < res.data.length; i++) {
           device = res.data[i].device;
@@ -48,12 +62,16 @@ class Main extends React.Component {
           numsold += res.data[i].numsold;
         }
 
+        console.log(total, numsold)
+
         self.setState({
           device: device,
+          prevTotal: prevTotal,
           total: total,
+          prevNumsold: prevNumsold,
           numsold: numsold,
         })
-      }) }, 500);
+      }) }, 3000);
   }
 
   componentWillUnmount() {
@@ -61,20 +79,58 @@ class Main extends React.Component {
   }
 
 // Clear user token and profile data from localStorage
-  logout(e) {
+  logout() {
     localStorage.removeItem('token');
     swal({ text: 'You are now logged out.', showConfirmButton: false, timer: 1500});
     this.props.history.replace('/');
   }
 
+//Conditional rendering for sale button on onClick
+  showSales() {
+    if(this.state.showSales === false) {
+      this.setState({
+        showSales: true,
+      })
+    } else {
+      this.setState({
+        showSales: false,
+      })
+    }
+    console.log('cliced')
+  }
+
   render() {
     return (
       <div>
-        <button>Sales</button>
-        <button onClick={ this.logout }>Logout</button>
-        <h1>DEVICE = {this.state.device}</h1>
-        <h1>TOTAL = {this.state.total}</h1>
-        <h1>NUMSOLD = {this.state.numsold}</h1>
+        <button onClick= {this.showSales}>Sales</button>
+        <button id="logout" onClick={this.logout}>Logout</button>
+          {this.state.showSales ? <div id="title" className="machinesale">
+            <img className="machine" src={'../../../images/whitelogo.png'} />
+            <br /><br />
+            <h1 className="machinename">MR-2</h1>
+            <br />
+            <CountUp
+              className="countup"
+              start={this.state.total}
+              end={this.state.total}
+              useEasing={true}
+              useGrouping={true}
+              separator=","
+              decimals={2}
+              prefix="Total Sales: $"
+            />
+            <br />
+            <CountUp
+              className="countup"
+              start={this.state.numsold}
+              end={this.state.numsold}
+              useEasing={true}
+              useGrouping={true}
+              separator=","
+              decimals={0}
+              prefix="Total Items Sold: "
+            />
+          </div> : null}
       </div>
     )
   }
