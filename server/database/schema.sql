08f6efac4fc2bdb17a7b6f451778e8e1e261afe4
hbvbot <bjviloria104@gmail.com>
2018-07-11 12:13:58 -0700

    Update schema

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,8 @@
-CREATE DATABASE mrvms;
-
-USE mrvms;
-
 CREATE TABLE sales (
-  saleid SERIAL PRIMARY KEY,
-  machine VARCHAR(255),
-  card_reader VARCHAR(255),
-  total_sale VARCHAR(255),
-  num_sold INT,
-  email VARCHAR(255),
+  sale_id       SERIAL PRIMARY KEY,
+  machine       VARCHAR(50),
+  card_reader   VARCHAR(50),
+  total_sale    VARCHAR(50),
+  num_sold      INT,
+  email         VARCHAR(50)
 );
