275a5043436b0ba6f4d3ca7828766ba027fa4a13
hbvbot <bjviloria104@gmail.com>
2018-07-12 16:51:20 -0700

    Attempt Heroku fix

--- server/index.js
+++ server/index.js
@@ -8,17 +8,20 @@ require('dotenv').config();
 
 let app = express();
 app.use(express.static(__dirname + '/../client/dist'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/', router)
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use(cors({
   allowedHeaders: 'Content-Type, Authorization',
   methods: ['GET, POST, PUT, DELETE, OPTIONS'],
