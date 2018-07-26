677972b537ba3adb383f4570e978adf736cb9b55
hbvbot <bjviloria104@gmail.com>
2018-07-26 02:03:52 -0700

    Fix ManageEvents

--- client/src/components/ManageEvents/ManageEvents.jsx
+++ client/src/components/ManageEvents/ManageEvents.jsx
@@ -5,8 +5,11 @@ class ManageEvents extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      showCreateEvent: false,
+      showEditEvent: false,
     }
+    this.showCreateEvent = this.showCreateEvent.bind(this);
+    this.showEditEvent = this.showEditEvent.bind(this);
   }
 
   render() {
