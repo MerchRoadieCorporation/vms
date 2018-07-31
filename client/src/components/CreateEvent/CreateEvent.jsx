23ad659a490c768edaf53a4c1adec3f0bc7c6715
hbvbot <bjviloria104@gmail.com>
2018-07-31 13:14:03 -0700

    Fix CreateEvent calendar

--- client/src/components/CreateEvent/CreateEvent.jsx
+++ client/src/components/CreateEvent/CreateEvent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import $ from 'jquery';
-import DayPicker, { DateUtils } from 'react-day-picker';
+import DayPicker from 'react-day-picker';
 import swal from 'sweetalert2';
 import TimePicker from 'rc-time-picker';
 import moment from 'moment';
@@ -10,37 +10,25 @@ class CreateEvent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      from: undefined,
-      to: undefined,
+      date: null,
       startTime: undefined,
       endTime: undefined,
     }
     this.handleDayClick = this.handleDayClick.bind(this);
-    this.handleResetClick = this.handleResetClick.bind(this);
     this.handleCreateEvent = this.handleCreateEvent.bind(this);
     this.createEvent = this.createEvent.bind(this);
     this.startTimeChange = this.startTimeChange.bind(this);
     this.endTimeChange = this.endTimeChange.bind(this);
   }
 
-  getInitialState() {
+  handleDayClick(day, { selected }) {
     this.setState({
-      from: undefined,
-      to: undefined,
-    })
-  }
-
-  handleDayClick(day) {
-    const range = DateUtils.addDayToRange(day, this.state);
-    this.setState(range);
-  }
-
-  handleResetClick() {
-    this.setState(this.getInitialState());
+      date: selected ? undefined : day,
+    });
   }
 
   handleCreateEvent() {
-    if(this.state.from === undefined) {
+    if(this.state.date === undefined) {
       swal({
         type: 'error',
         title: 'Error',
@@ -49,8 +37,7 @@ class CreateEvent extends React.Component {
         timer: 1500
       })
     } else {
-        this.sendDates();
-        this.props.showFilteredSales();
+        console.log('click')
     }
   }
 
@@ -60,7 +47,7 @@ class CreateEvent extends React.Component {
     if(this.state.to) {
       to = this.state.to.toLocaleDateString();
     }
-    const dates = [this.state.from.toLocaleDateString(), to]
+    const dates = [this.state.date.toLocaleDateString(), to]
     const name = $('#EventName').val();
     const email = localStorage.email;
   }
@@ -80,34 +67,21 @@ class CreateEvent extends React.Component {
   render() {
     const format = 'h:mm a';
     const now = moment().hour(0).minute(0);
-    const { from, to } = this.state;
-    const modifiers = { start: from, end: to };
     return (
       <div>
       <label><input id="EventName" style={{ width: 300 }} className="login" type="text" name="eventname" />Event Name:</label>
         <div className="calendar">
           <p>
-            {!from && !to && 'Please select a date.'}
-            {from && !to && '*Optional* Please select the last date.'}
-            {from &&
-              to &&
-              `Selected from ${from.toLocaleDateString()} to
-                  ${to.toLocaleDateString()}`}{' '}
-            {from &&
-              to && (
-                <button className="link" onClick={this.handleResetClick}>
-                  Reset
-                </button>
-              )}
+            {this.state.date ? `${this.state.date.toLocaleDateString()} selected` : 'Please select a day'}
           </p>
           <DayPicker
             className="Selectable"
-            numberOfMonths={this.props.numberOfMonths}
-            selectedDays={[from, { from, to }]}
-            modifiers={modifiers}
+            selectedDays={this.state.date}
             onDayClick={this.handleDayClick}
-          /><br />
-          Start Time:<br /><TimePicker
+          />
+          <br />
+          Start Time:
+          <br /><TimePicker
             showSecond={false}
             defaultValue={now}
             className="xxx"
@@ -115,18 +89,20 @@ class CreateEvent extends React.Component {
             format={format}
             use12Hours
             inputReadOnly
-           /><br /><br />
-           End Time:<br /><TimePicker
-             showSecond={false}
-             defaultValue={now}
-             className="xxx"
-             onChange={this.endTimeChange}
-             format={format}
-             use12Hours
-             inputReadOnly
-            /><br /><br />
+           />
+          <br /><br />
+         End Time:
+         <br /><TimePicker
+           showSecond={false}
+           defaultValue={now}
+           className="xxx"
+           onChange={this.endTimeChange}
+           format={format}
+           use12Hours
+           inputReadOnly
+          /><br /><br />
           <button onClick={this.handleCreateEvent}>Create Event</button>
-          </div>
+        </div>
       </div>
     )
   }
